42088c8282bfd59d37da0417c17ae9039acbfd27
guntamania <medaka.mp3@gmail.com>
2023-05-22 16:32:49 +0900

    Fix init el

--- init.el
+++ init.el
@@ -1,1 +1,59 @@
-/Users/hyamada/dotemacs/init.el
+;; <leaf-install-code>
+(eval-and-compile
+  (customize-set-variable
+   'package-archives '(("org" . "https://orgmode.org/elpa/")
+                       ("melpa" . "https://melpa.org/packages/")
+                       ("gnu" . "https://elpa.gnu.org/packages/")))
+  (package-initialize)
+  (unless (package-installed-p 'leaf)
+    (package-refresh-contents)
+    (package-install 'leaf))
+
+  (leaf leaf-keywords
+    :ensure t
+    :init
+    ;; optional packages if you want to use :hydra, :el-get, :blackout,,,
+    (leaf hydra :ensure t)
+    (leaf el-get :ensure t)
+    (leaf blackout :ensure t)
+
+    :config
+    ;; initialize leaf-keywords.el
+    (leaf-keywords-init)))
+;; </leaf-install-code>
+
+(global-linum-mode 1)
+
+(global-set-key "\C-h" 'delete-backward-char)
+(define-key global-map "\C-o" 'other-window)
+
+(fset 'yes-or-no-p 'y-or-n-p)
+
+(show-paren-mode t) ; 対応する括弧を光らせる。
+(transient-mark-mode t) ; 選択部分のハイライト
+(global-font-lock-mode t)
+
+(custom-set-variables '(line-number-mode t) '(column-number-mode t) ) ;行数表示
+;; タイトルバーにファイル名を表示する
+(setq frame-title-format (format "%%f - emacs" (system-name)))
+(display-time)  ; 時間を表示
+
+;; Stop Auto Backup
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+
+(global-auto-revert-mode)
+
+(setq indent-line-function 'indent-relative-maybe) ; 前と同じ行の幅にインデント
+(setq completion-ignore-case t) ; file名の補完で大文字小文字を区別しない
+(setq inhibit-startup-message t) ;スタートアップ非表示
+(setq initial-scratch-message "") ;スクラッチのメッセージ非表示
+
+(leaf leaf
+  :config
+  (leaf leaf-convert :ensure t)
+  (leaf leaf-tree
+    :ensure t
+    :custom ((imenu-list-size . 30)
+             (imenu-list-position . 'left))))
+
